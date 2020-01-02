30a1e4fb847d5b43c178940ff098f50b75c8411c
R Avenash <avenash.r15@iiits.in>
2020-01-02 15:06:23 +0530

    Add files via upload
    Language part added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
   /**
-   * Queries BigQuery and retrieves the results
+   * Queries BigQuery and retrieves the result of notes
    * @param {String} subject 
    * @param {Integer} classValue 
    * @param {String} chapterName 
@@ -27,11 +27,31 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
       query: query,
       location: 'US',
     };
-
     const [job] = await bigquery.createQueryJob(options);
     console.log(`Job ${job.id} started.`);
     const [rows] = await job.getQueryResults();
-    console.log('Rows:');
+    console.log('Rows returned');
+    rows.forEach(row => console.log(row));
+    return rows
+  }
+
+  /**
+   * Queries BigQuery and retrieves the result of questions
+   * @param {String} subject 
+   * @param {Integer} classValue 
+   * @param {String} chapterName 
+   */
+  async function questionsQuery(subject, classValue, chapterName) {
+    const query = 'SELECT questionNumber, question, option1, option2, option3, option4, answer FROM \`mercurial-bruin-254505.chatbot.questionBank\` WHERE Subject = "' + subject + '" and chapterName = "' + chapterName + '" and Class = ' + classValue + ' order by questionNumber';
+    console.log("Query: ", query)
+    const options = {
+      query: query,
+      location: 'US',
+    };
+    const [job] = await bigquery.createQueryJob(options);
+    console.log(`Job ${job.id} started.`);
+    const [rows] = await job.getQueryResults();
+    console.log('Rows returned');
     rows.forEach(row => console.log(row));
     return rows
   }
@@ -42,37 +62,60 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
    */
   function doubtSolver(agent) {
     const uuidv1 = require('uuid/v1')
-    var subject = agent.parameters["subjects"];
-    var classValue = agent.parameters["class"];
-    var doubtValue = agent.parameters["doubtValue"];
-    var text = 'Sure, your ticket number issued is ' + uuidv1() + '. Your doubt \' ' + doubtValue + '\' will be clarified. Solution will be sent in your registered mail id.'
-    agent.add(text);
+    console.log("entered doubt solver");
+    if (agent.locale == 'en-us'){
+      var subject = agent.parameters["subjects"];
+      var classValue = agent.parameters["class"];
+      var doubtValue = agent.parameters["doubtValue"];
+      console.log("entered english");
+      var text = 'Sure, your ticket number issued is ' + uuidv1() + '. Your doubt \' ' + doubtValue + '\' will be clarified. Solution will be sent in your registered mail id.'
+      agent.add(text);
+    }
+    else if (agent.locale == 'hi'){
+      var subject = agent.parameters["subjects"];
+      var classValue = agent.parameters["hindiClass"];
+      var doubtValue = agent.parameters["doubtValue"];
+      console.log("entered hindi");
+      var hindiText = 'धन्यवाद, आपका टिकट नंबर '+ uuidv1() +' है! आपके डाउट का समाधान आपके दर्ज कराये मेल पे भेज दिया जायेगा।'
+      agent.add(hindiText);
+    }
   }
 
   /**
    * Intent handler for notes required by user
    * @param {dialogflow agent} agent 
    */
-  function noteRetriever(agent) {
+  async function noteRetriever(agent) {
     var subject = agent.parameters["subjects"];
     var classValue = agent.parameters["class"];
     var chapterName = agent.parameters["chapterName"];
-    var rows = await notesQuery(subject, classValue, chapterName)
-    console.log("rows: ", rows)
-    console.log("link : ", rows.Link)
-    var link = rows['Link']
-    console.log("link 2 : ", link)
-    var text = 'Sure, here is the link of notes in pdf version. '+link
+    await notesQuery(subject, classValue, chapterName).then( output=> {
+      console.log("output og notes:",output)
+      var link = output[0].Link
+      var text = 'Sure, here is the link of notes in pdf version. '+link
+      console.log("text: ", text)
+      agent.add(text)
+    })
   }
 
   /**
    * Intent handler for taking quiz by user
    * @param {dialogflow agent} agent 
    */
-  function assesment(agent) {
+  async function assesment(agent) {
     var subject = agent.parameters["subjects"];
     var classValue = agent.parameters["class"];
     var chapterName = agent.parameters["chapterName"];
+    await questionsQuery(subject, classValue, chapterName).then( output=> {
+      console.log("output of question bank:",output)
+      for (let i = 0; i < output.length; i++){
+
+      }
+      // var link = output[0].Link
+      var text = 'Sure, There will be total 5 questions. Please type START when you are ready. '
+      // console.log("text: ", text)
+      agent.add(text)
+    })
   }
 
   /**
